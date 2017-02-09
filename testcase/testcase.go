5c7858e4818b76952ae2f325a084d156bca74453
Lucas Bremgartner <lucas.bremgartner@swisscom.com>
2017-02-09 13:13:31 +0100

    Slice for `input` and `expected` in `testcases`

--- testcase/testcase.go
+++ testcase/testcase.go
@@ -70,14 +70,14 @@ type TestCaseSet struct {
 // into the Logstash process and an expected event which is compared
 // to the actual event produced by the Logstash process.
 type TestCase struct {
-	// InputLine contains the line of input that should be fed
+	// InputLines contains the lines of input that should be fed
 	// to the Logstash process.
-	InputLine string `json:"input"`
+	InputLines []string `json:"input"`
 
-	// ExpectedEvent contains an expected event to be
-	// compared to the actual event produced by the Logstash
+	// ExpectedEvents contains a slice of expected events to be
+	// compared to the actual events produced by the Logstash
 	// process.
-	ExpectedEvent logstash.Event `json:"expected"`
+	ExpectedEvents []logstash.Event `json:"expected"`
 }
 
 // ComparisonError indicates that there was a mismatch when the
@@ -126,8 +126,8 @@ func New(reader io.Reader) (*TestCaseSet, error) {
 	}
 	sort.Strings(tcs.IgnoredFields)
 	for _, tc := range tcs.TestCases {
-		tcs.InputLines = append(tcs.InputLines, tc.InputLine)
-		tcs.ExpectedEvents = append(tcs.ExpectedEvents, tc.ExpectedEvent)
+		tcs.InputLines = append(tcs.InputLines, tc.InputLines...)
+		tcs.ExpectedEvents = append(tcs.ExpectedEvents, tc.ExpectedEvents...)
 	}
 	return &tcs, nil
 }
